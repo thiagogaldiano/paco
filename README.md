a701ecabfc24bdba046ee6278b567f9264c3f8b1
Thiago <ti@direitodeouvir.com.br>
2021-04-17 11:19:19 -0300

    Init

---
+++ README.md
@@ -0,0 +1,10 @@
+cp .env.example .env </br>
+Configurar o database </br><br/>
+composer install<br/>
+php artisan key:generate<br/>
+php artisan migrate --seed<br/>
+npm install<br/>
+npm run dev<br/><br/>
+Admin User Padrão<br/>
+E-mail: admin@admin.com<br/>
+Password: 12345678
---
+++ resources/views/coins/fields.blade.php
@@ -0,0 +1,17 @@
+<!-- Name Field -->
+<div class="form-group col-sm-6">
+    {!! Form::label('name', 'Nome:') !!}
+    {!! Form::text('name', null, ['class' => 'form-control','maxlength' => 255,'maxlength' => 255]) !!}
+</div>
+
+<!-- Code Field -->
+<div class="form-group col-sm-6">
+    {!! Form::label('code', 'Código:') !!}
+    {!! Form::text('code', null, ['class' => 'form-control','maxlength' => 255,'maxlength' => 255]) !!}
+</div>
+
+<!-- Submit Field -->
+<div class="form-group col-sm-12">
+    {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
+    <a href="{{ route('coins.index') }}" class="btn btn-secondary">Cancel</a>
+</div>
